d146e11a5953de4c0137e93f0b0a23932862f1e9
sanya jain <sanyajain033@gmail.com>
2024-09-08 22:12:31 +0530

    frontend completed

---
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+import './PlayVideo.css'
+import video1 from './video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import jack from '../../assets/jack.png'
+import user_profile from '../../assets/user_profile.jpg'
+
+const PlayVideo = ({videoId}) => {
+    return (
+        <div className="play-video">
+        {/*<video src={video1} controls autoPlay muted></video> */}
+       <iframe src={`https://www.youtube.com/embed/${videoId}?autoplay=1`} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <h3>The best way to learn coding</h3>
+        <div className="play-video-info">
+            <p>1525 views &bull; 2 days ago</p>
+            <div>
+                <span><img src={like} alt="" /> 125</span>
+                <span><img src={dislike} alt="" /> 2</span>
+                <span><img src={share} alt="" /> Share</span>
+                <span><img src={save} alt="" /> Save</span>
+            </div>
+        </div>
+        <hr/>
+        <div className="publisher">
+            <img src={jack} alt="" />
+            <div>
+                <p>Greatstack</p>
+                <span>1M Subscribers</span>
+            </div>
+            <button>Subscribe!</button>
+        </div>
+        <div className="vid-description">
+            <p>Channel that makes learning Easy</p>
+            <p>Sybscribe Greatstack to watch more tutorials on web development</p>
+            <hr/>
+            <h4>130 Comments</h4>
+            <div className="comment">
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Jack Nicholson <span>1 day ago</span></h3>
+                    <p>Hello, my best lecture on you tube that i have ever meet, that is an amazing tutorial.
+                         I recommend you make a tutorial on how to host a website online.
+                          Thanks for you great effort to share your knowledge with us</p>
+                <div className="comment-action">
+                    <img src={like} alt="" />
+                    <span>244</span>
+                    <img src={dislike} alt="" />
+                </div>
+                </div>
+            </div>
+            <div className="comment">
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Jack Nicholson <span>1 day ago</span></h3>
+                    <p>Hello, my best lecture on you tube that i have ever meet, that is an amazing tutorial.
+                         I recommend you make a tutorial on how to host a website online. 
+                         Thanks for you great effort to share your knowledge with us</p>
+                <div className="comment-action">
+                    <img src={like} alt="" />
+                    <span>244</span>
+                    <img src={dislike} alt="" />
+                </div>
+                </div>
+            </div>
+            <div className="comment">
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Jack Nicholson <span>1 day ago</span></h3>
+                    <p>Hello, my best lecture on you tube that i have ever meet, that is an amazing tutorial. 
+                        I recommend you make a tutorial on how to host a website online.
+                         Thanks for you great effort to share your knowledge with us</p>
+                <div className="comment-action">
+                    <img src={like} alt="" />
+                    <span>244</span>
+                    <img src={dislike} alt="" />
+                </div>
+                </div>
+            </div>
+            <div className="comment">
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Jack Nicholson <span>1 day ago</span></h3>
+                    <p>Hello, my best lecture on you tube that i have ever meet, that is an amazing tutorial.
+                         I recommend you make a tutorial on how to host a website online. 
+                         Thanks for you great effort to share your knowledge with us</p>
+                <div className="comment-action">
+                    <img src={like} alt="" />
+                    <span>244</span>
+                    <img src={dislike} alt="" />
+                </div>
+                </div>
+            </div>
+            <div className="comment">
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Jack Nicholson <span>1 day ago</span></h3>
+                    <p>Hello, my best lecture on you tube that i have ever meet, that is an amazing tutorial. 
+                        I recommend you make a tutorial on how to host a website online. 
+                        Thanks for you great effort to share your knowledge with us</p>
+                <div className="comment-action">
+                    <img src={like} alt="" />
+                    <span>244</span>
+                    <img src={dislike} alt="" />
+                </div>
+                </div>
+            </div>
+            <div className="comment">
+                <img src={user_profile} alt="" />
+                <div>
+                    <h3>Jack Nicholson <span>1 day ago</span></h3>
+                    <p>Hello, my best lecture on you tube that i have ever meet, that is an amazing tutorial.
+                         I recommend you make a tutorial on how to host a website online. 
+                         Thanks for you great effort to share your knowledge with us</p>
+                <div className="comment-action">
+                    <img src={like} alt="" />
+                    <span>244</span>
+                    <img src={dislike} alt="" />
+                </div>
+                </div>
+            </div>
+        </div>
+        </div>
+    )
+}
+
+export default PlayVideo
